f82efdd6de8ae6a44428da2745f4da35cef0b40c
v_dhuiyang <v_dhuiyang@tencent.com>
2020-03-08 22:43:25 +0800

    react-redux applyMiddleware practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'antd/dist/antd.css'
+import {Input,Button,List} from "antd";
+import store from './store/index.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class TodoList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        this.changeInputValue = this.changeInputValue.bind(this)
+        this.storeChange=this.storeChange.bind(this)
+        this.clickButton=this.clickButton.bind(this)
+        store.subscribe(this.storeChange)
+    }
+  render() {
+    return (
+        <div style={{margin:'50px'}}>
+          <div>
+            <Input
+                placeholder={this.state.inputValue}
+                style={{width:'250px',marginRight:'10px'}}
+                onChange={this.changeInputValue}
+                value={this.state.inputValue}
+            />
+            <Button type="primary" onClick={this.clickButton}>增加</Button>
+          </div>
+          <div style={{margin:'10px',width:'300px'}}>
+            <List
+                bordered
+                dataSource={this.state.list}
+                renderItem={(item,index)=>(
+                    <List.Item
+                        onClick={()=>{this.deleteItem(index)}}>
+                        {item}
+                    </List.Item>)}
+            />
+          </div>;
+        </div>
+    )
+  }
+  changeInputValue(e){
+        const action={
+            type:'changeInput',
+            value:e.target.value
+
+        };
+      // console.log(e.target.value)
+        store.dispatch(action)
+    }
+    storeChange(){
+        this.setState(store.getState())
+    }
+    clickButton(){
+        const action={
+            type:'addItem'
+        }
+        store.dispatch(action)
+    }
+    deleteItem(index){
+        const action={
+            type:'deleteItem',
+            index
+        }
+        store.dispatch(action)
+    }
 }
-
-export default App;
